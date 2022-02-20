abd24cb82782338c128932db32bcf465bbc3c8f9
Maximilian Øystå Lloyd <me@maxlloyd.no>
2022-02-20 19:27:56 +0100

    Putting custom buffer setup on the shelf

--- lua/mivi/init.lua
+++ lua/mivi/init.lua
@@ -1,22 +1,56 @@
+local popup = require("plenary.popup")
+
+
 local M = {}
+local config = {}
 
-M.example = function()
-    print("HI")
+M.create_window = function(_opts)
+    local opts = _opts or {}
+
+
+    local bufnr = vim.api.nvim_create_buf(false, true)
+
+    local borderchars = opts.borderchars or { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+    local width = config.width or 60
+    local height = config.height or 10
+
+    local Mivi_win_id, win = popup.create(bufnr, {
+        title = "Mivi",
+        highlight = "MiviWindow",
+        borderchars = borderchars,
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        width = 60,
+        height = 10,
+    })
+
+    local response = {
+        bufnr = bufnr,
+        win_id = Mivi_win_id,
+    }
+
+    return response
 end
 
-project_workbench_bufnr = vim.api.nvim_create_buf(false, true)
-project_workbench_initialized = false
-
-M.setup = function(opts)
-    local ui = vim.api.nvim_list_uis()[1]
-
-    local width = math.floor(ui.width * 0.5)
-    local height = math.floor(ui.height * 0.5)
-
-    print(width, height)
-    print("Options", opts)
+M.close = function(win_id)
+    vim.api.nvim_win_close(win_id, true)
 end
 
-print(M.setup())
+local items = { "first", "second", "third" }
+local win_info = M.create_window()
 
-return M
+vim.api.nvim_win_set_option(win_info.win_id, "number", true)
+vim.api.nvim_win_set_height(win_info.win_id, 10)
+
+
+vim.api.nvim_buf_set_option(win_info.bufnr, "buftype", "acwrite")
+vim.api.nvim_buf_set_option(win_info.bufnr, "bufhidden", "wipe")
+
+vim.api.nvim_buf_set_lines(win_info.bufnr, 0, -1, true, items)
+
+vim.keymap.set("n", "mc", function()
+    print("Calling close")
+    M.close(win_info.win_id)
+end, { noremap = true, silent = true })
+
