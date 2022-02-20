4372c57e745c6ade2c4270485534ccdd1abbd511
Maximilian Øystå Lloyd <me@maxlloyd.no>
2022-02-20 21:43:10 +0100

    Getting the hang of things

--- lua/mivi/init.lua
+++ lua/mivi/init.lua
@@ -1,16 +1,24 @@
 local popup = require("plenary.popup")
-
+local json = require("plenary.json")
+local curl = require("plenary.curl")
+-- local http_request = require "http.request"
 
 local M = {}
 local config = {}
 
-M.create_window = function(_opts)
-    local opts = _opts or {}
+M.setup = function(options)
+    local default_options = {
 
+    }
+    config = options
+end
 
+M.create_window = function(_opts, items)
+    local options = {} or _opts
     local bufnr = vim.api.nvim_create_buf(false, true)
 
-    local borderchars = opts.borderchars or { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, items)
+    local borderchars = options.borderchars or { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
 
     local width = config.width or 60
     local height = config.height or 10
@@ -22,8 +30,14 @@ M.create_window = function(_opts)
         line = math.floor(((vim.o.lines - height) / 2) - 1),
         col = math.floor((vim.o.columns - width) / 2),
         width = 60,
-        height = 10,
+        minheight = 20,
+        -- height = 10,
     })
+
+    vim.api.nvim_buf_set_option(bufnr, "buftype", "acwrite")
+    vim.api.nvim_buf_set_option(bufnr, "bufhidden", "wipe")
+    vim.api.nvim_buf_set_option(bufnr, "modifiable", false)
+
 
     local response = {
         bufnr = bufnr,
@@ -37,17 +51,22 @@ M.close = function(win_id)
     vim.api.nvim_win_close(win_id, true)
 end
 
-local items = { "first", "second", "third" }
-local win_info = M.create_window()
+res = curl.request({
+    url = "https://jsonplaceholder.typicode.com/todos/1",
+    method = "get",
+    accept = "application/json"
+})
 
-vim.api.nvim_win_set_option(win_info.win_id, "number", true)
-vim.api.nvim_win_set_height(win_info.win_id, 10)
+local result = vim.fn.json_decode(res.body)
+local items = { "First", "second", "third" }
+local win_info = M.create_window({}, { result.title })
 
-
-vim.api.nvim_buf_set_option(win_info.bufnr, "buftype", "acwrite")
-vim.api.nvim_buf_set_option(win_info.bufnr, "bufhidden", "wipe")
-
-vim.api.nvim_buf_set_lines(win_info.bufnr, 0, -1, true, items)
+-- local headers, stream = assert(http_request.new_from_uri("https://jsonplaceholder.typicode.com/todos/1"):go())
+-- local body = assert(stream:get_body_as_string())
+-- if headers:get ":status" ~= "200" then
+--     error(body)
+-- end
+-- print(body)
 
 vim.keymap.set("n", "mc", function()
     print("Calling close")
